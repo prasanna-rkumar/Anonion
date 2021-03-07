d352e6e07ae7076b81edddf6a6c2513c4faffb53
Prasanna Kumar <prasannasrk07@gmail.com>
2021-03-07 20:19:43 +0530

    responses style

--- pages/a/[anonionID].js
+++ pages/a/[anonionID].js
@@ -65,9 +65,9 @@ function AnonionPage({ isOwner, anonion, displayName, url }) {
 			</Head>
 			<div className="max-w-screen-xl m-auto px-2">
 				<Header email={AuthUser.email} signOut={AuthUser.signOut} />
-				<main className="w-11/12 m-auto max-w-screen-md">
+				<main className="w-11/12 m-auto max-w-screen-xl">
 					{isOwner ? <></> : <div className="text-gray-600 text-xl font-medium pb-2 pl-2">{displayName}'s Question</div>}
-					<div style={{ boxShadow: "0 2px 13px -2px rgb(0 0 0 / 10%)" }} className="bg-white w-full rounded-lg p-4 text-center">
+					<div style={{ boxShadow: "0 2px 13px -2px rgb(0 0 0 / 10%)" }} className="bg-white w-full rounded-lg p-4 text-center max-w-screen-md m-auto mb-4">
 						<span className="text-4xl font-bold">{anonion.question}</span>
 						<div className="flex flex-row justify-center m-3">
 							<a href={"https://twitter.com/intent/tweet?text=" + message + "&url=" + shareURL}>
@@ -77,29 +77,18 @@ function AnonionPage({ isOwner, anonion, displayName, url }) {
 								<img className="w-10 px-2 cursor-pointer" src="/copy.png" alt="Copy" />
 							</CopyToClipboard>
 						</div>
-						{isOwner ?
-							<div>
-								{responses.length > 0 ? <div style={{
-									width: "100%",
-									flex: 1,
-									padding: "1rem 0 2rem 0",
-									display: "grid",
-									gridGap: 20,
-									alignItems: "baseline",
-									gridTemplateColumns: "repeat(auto-fill,minmax(45%,1fr))"
-								}} >
-									{responses.map((value, index) => {
-										return <div key={value.id} style={{
-											backgroundColor: "#bfd1ec",
-											color: "#00327c"
-										}} className="text-lg font-medium cursor-pointer rounded-lg shadow-xl float-left p-4">{value.answer}</div>
-									})}
-								</div>
-									: <div className="m-2 text-md text-gray-600 font-semibold">No Answers</div>}
-							</div> : <>
-								{isOwner ? <></> : <DynamicComponentWithCustomLoading anonionID={anonionID} onSuccess={() => toast.success("Answer sent to " + displayName)} onError={() => toast.error("Something went wrong!")} />}
-							</>}
 					</div>
+					{isOwner ? responses.length > 0 ? <div className="w-full flex-1 py-1 grid gap-5 items-baseline grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+						{responses.map((value, index) => {
+							return <div key={value.id} style={{
+								backgroundColor: "#bfd1ec",
+								color: "#00327c"
+							}} className="text-lg font-medium cursor-pointer rounded-lg shadow-xl float-left p-4">{value.answer}</div>
+						})}
+					</div>
+						: <div className="m-2 text-md text-gray-600 font-semibold">No Answers</div> : <>
+						{isOwner ? <></> : <DynamicComponentWithCustomLoading anonionID={anonionID} onSuccess={() => toast.success("Answer sent to " + displayName)} onError={() => toast.error("Something went wrong!")} />}
+					</>}
 				</main>
 				<ToastContainer
 					autoClose={2000}
